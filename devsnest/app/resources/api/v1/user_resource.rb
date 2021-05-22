57231cf456bb865ac434185d94a315e12fb8f1b2
priyamishra2903 <mishrapriya9999@gmail.com>
2021-05-22 16:09:19 +0530

    User profile

--- devsnest/app/resources/api/v1/user_resource.rb
+++ devsnest/app/resources/api/v1/user_resource.rb
@@ -4,8 +4,9 @@ module Api
   module V1
     class UserResource < JSONAPI::Resource
       attributes :email, :name, :password, :discord_id, :web_active, :username, :score, :discord_active, :batch, :grad_status, :grad_specialization, :grad_year
-      attributes :group_id
-      # attributes :college_id
+      attributes :group_id, :group_name 
+      attributes :college
+      attributes :easy_solved, :medium_solved, :hard_solved
 
       def fetchable_fields
         if context[:user].nil? || context[:user].id == @model.id
@@ -20,11 +21,36 @@ module Api
         member = GroupMember.where(user_id: context[:user].id).first
         member.present? ? member.group_id : nil
       end
+
+      def group_name
+        return nil if context[:user].nil?
+        member = GroupMember.where(user_id: context[:user].id).first 
+        groupId = member.present? ? member.group_id : nil
+        group =  Group.where(id: groupId).first
+        group.present? ? group.name : nil
+      end
+
+      def college
+        return nil if context[:user].nil?
+        member = College.where(user_id: context[:user].id).first       
+        member.present? ? member.name : nil
+      end
+
+      def easy_solved
+        return nil if context[:user].nil?
+        Submission.count_solved(context[:user].id, "easy")
+      end   
+
+      def medium_solved
+        return nil if context[:user].nil?
+        Submission.count_solved(context[:user].id, "medium")
+      end  
+
+      def hard_solved
+        return nil if context[:user].nil?
+        Submission.count_solved(context[:user].id, "hard")
+      end  
+
     end
-      
-      def college_id
-        
-
-
   end
 end
