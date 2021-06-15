aa14f42c6663f555bc7d6748e215decdf717ea2d
kalyug <yug.gurnani091@gmail.com>
2021-06-16 02:12:50 +0530

    🐛 Migration bug solved

--- devsnest/db/migrate/20210604064000_add_user_type_to_users.rb
+++ devsnest/db/migrate/20210604064000_add_user_type_to_users.rb
@@ -1,5 +1,7 @@
 class AddUserTypeToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users  , :user_type, :integer,default: 0
+    add_column :users, :user_type, :integer,default: 0
+    add_column :users, :bot_token, :string
+    add_column :users, :google_id, :string
   end
 end
