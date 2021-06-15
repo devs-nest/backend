8e738f5076c3941d712d7a577c3e777eae25a9fe
kalyug <yug.gurnani091@gmail.com>
2021-06-16 02:20:45 +0530

    🐛 Migration bug solved

--- devsnest/db/migrate/20210604064000_add_user_type_to_users.rb
+++ devsnest/db/migrate/20210604064000_add_user_type_to_users.rb
@@ -1,7 +1,5 @@
 class AddUserTypeToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :user_type, :integer,default: 0
-    add_column :users, :bot_token, :string
-    add_column :users, :google_id, :string
   end
 end
--- devsnest/db/schema.rb
+++ devsnest/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_04_064000) do
+ActiveRecord::Schema.define(version: 2021_06_05_145138) do
 
   create_table "batches", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.integer "owner_id"
