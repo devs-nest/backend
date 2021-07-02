061b702969fdab52ebadec5ee098eb53f4739236
LakshitXD <lakshitk6666@gmail.com>
2021-07-02 23:19:51 +0530

    ✨ added submission link feature

--- devsnest/app/models/frontend_submission.rb
+++ devsnest/app/models/frontend_submission.rb
@@ -2,12 +2,4 @@
 
 # frontend submission model
 class FrontendSubmission < ApplicationRecord
-  def self.create_submission(user_id, content_id, submission_link)
-    submission = FrontendSubmission.find_by(user_id: user_id, content_id: content_id, submission_link: submission_link)
-
-    unless submission.present?
-      submission = FrontendSubmission.create(user_id: user_id, content_id: content_id, submission_link: submission_link)
-    end
-    submission
-  end
 end
