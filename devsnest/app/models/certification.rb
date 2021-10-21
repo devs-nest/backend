8ff3424865101a6f719b52e95e84d9dfa40a296a
Yug-gurnani <yug.gurnani091@gmail.com>
2021-10-21 18:47:20 +0530

    Certification revamp and rspecs

--- devsnest/app/models/certification.rb
+++ devsnest/app/models/certification.rb
@@ -8,5 +8,24 @@ class Certification < ApplicationRecord
     self.cuid = SecureRandom.base64(8).gsub('/', '_').gsub(/=+$/, '')
   end
 
-  # enum certificate_type: %i[course_dsa course_frontend course_backend course_dsa_frontend_backend community_batch_leader community_student_mentor community_moderator]
+  def self.type_to_description(user_id, type)
+    name = User.find_by(id: user_id).name
+
+    case type
+    when 'course_dsa'
+      "This is to certify that #{name} has successfully completed the module on Data Structures and Algorithms of the Devsnest Course."
+    when 'course_frontend'
+      "This is to certify that #{name} has successfully completed the module on Front-End Web Development of the Devsnest Course."
+    when 'course_backend'
+      "This is to certify that #{name} has successfully completed the module on Back-End Web Development of the Devsnest Course."
+    when 'course_dsa_frontend_backend'
+      "This is to certify that #{name} has successfully completed the Devsnest course on DSA, Front-End and Back-End Web Development."
+    when 'community_batch_leader'
+      "This certificate of appreciation is awarded to #{name} for showcasing extra ordinary leadership skills throughout the Devsnest Course."
+    when 'community_student_mentor'
+      "This certificate of appreciation is awarded to #{name} for being an outstanding Student Mentor in the Devsnest Community."
+    when 'community_moderator'
+      "This certificate of appreciation is awarded to #{name} for being an outstanding Community Moderator in the Devsnest Community."
+    end
+  end
 end
