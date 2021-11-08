2bf2dbb23af7c9ec1de629bb936b4473bca5fe02
kaydee0502 <kshitijdhama513@gmail.com>
2021-11-09 00:27:42 +0530

    ac bug fixed

--- devsnest/app/controllers/api/v1/algo_submission_controller.rb
+++ devsnest/app/controllers/api/v1/algo_submission_controller.rb
@@ -27,7 +27,7 @@ module Api
         submission = AlgoSubmission.create(source_code: source_code, user_id: @current_user.id, language_id: lang_id, challenge_id: challenge_id, test_cases: {}, is_submitted: is_submitted,
                                            status: 'Pending')
         tokens = JSON.parse(AlgoSubmission.post_to_judgez({ 'submissions' => batch }))
-        puts tokens
+
         submission.ingest_tokens(tokens)
 
         submission.update(total_test_cases: total_test_cases)
@@ -36,7 +36,6 @@ module Api
 
       def callback
         submission_id = Judgeztoken.find_by(token: params[:token]).submission_id
-        puts "running CALLBACK ......... #{submission_id}"
         submission = AlgoSubmission.find_by(id: submission_id)
         submission.with_lock do
           res_hash = AlgoSubmission.prepare_test_case_result(params)
@@ -45,6 +44,7 @@ module Api
           submission.total_memory = submission.total_memory.to_i + res_hash['memory'].to_i
           submission.test_cases[params[:token]] = res_hash
           submission.passed_test_cases += 1 if params[:status][:id] == 3
+          submission.status = "Pending" if submission.status == "Accepted" && submission.total_test_cases != submission.passed_test_cases
           submission.save!
         end
       end
