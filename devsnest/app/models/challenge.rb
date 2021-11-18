fe4c8bc56a60eac2a80592606517af3f72ffe43e
kaydee0502 <kshitijdhama513@gmail.com>
2021-11-18 21:37:45 +0530

    lint fixes

--- devsnest/app/models/challenge.rb
+++ devsnest/app/models/challenge.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# algo challenge class
 class Challenge < ApplicationRecord
   enum difficulty: %i[easy medium hard]
   enum topic: %i[arrays strings hashmap tree matrix graph linkedlist stacks binarysearch queues heaps dynamicprogramming backtracking greedy maths]
--- devsnest/app/workers/judge_z_worker.rb
+++ devsnest/app/workers/judge_z_worker.rb
@@ -18,7 +18,7 @@ class JudgeZWorker
       submission.total_memory = submission.total_memory.to_i + res_hash["memory"].to_i
       submission.test_cases[token.to_s] = submission.test_cases[token.to_s].merge(res_hash)
       submission.passed_test_cases += 1 if res_hash["status_id"] == 3
-      submission.status = "Pending" if submission.status == "Accepted" && submission.total_test_cases != submission.passed_test_cases
+      submission.status = 'Pending' if submission.status == 'Accepted' && submission.total_test_cases != submission.passed_test_cases
       submission.save!
     end
   end
