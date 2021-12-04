c7819ebc8991dda8669fcfc6ba121bfcd50c6b8a
kaydee0502 <kshitijdhama513@gmail.com>
2021-12-04 14:22:48 +0530

    redis labs env setup

--- devsnest/config/initializers/leaderboard.rb
+++ devsnest/config/initializers/leaderboard.rb
@@ -3,7 +3,9 @@
 module LeaderboardDevsnest
   # Initialize leaderboard with redis
   class Initializer
-    RedisConnection = { redis_connection: Redis.new }
+    RedisConnection = { redis_connection: Redis.new(url: ENV['REDIS_HOST_LB'], password: ENV['REDIS_PASSWORD_LB'], db: ENV['REDIS_DB_LB']) }
+    # RedisConnection = {:host => 'redis://redis-16799.c264.ap-south-1-1.ec2.cloud.redislabs.com', :password => 'KWHz4C13IPs5sgQHYNs8YCuM7EsztXUh', :db => 'dn-prod-lb-redis', port: '16799'}
+
     LB = Leaderboard.new(ENV['REDIS_DB'], Devsnest::Application::REDIS_OPTIONS, RedisConnection)
   end
 end
