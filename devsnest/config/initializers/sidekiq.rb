912552363ca8ae9e9ab25391fa2114098a76c516
kaydee0502 <kshitijdhama513@gmail.com>
2021-12-04 14:22:48 +0530

    redis labs env setup

--- devsnest/config/initializers/sidekiq.rb
+++ devsnest/config/initializers/sidekiq.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'], namespace: "sidekiq_#{Rails.env}" }
+  config.redis = { url: ENV['REDIS_HOST_SQ'], password: ENV['REDIS_PASSWORD_SQ'], db: ENV['REDIS_DB_SQ'], namespace: "sidekiq_#{Rails.env}" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'], namespace: "sidekiq_#{Rails.env}" }
+  config.redis = { url: ENV['REDIS_HOST_SQ'], password: ENV['REDIS_PASSWORD_SQ'], db: ENV['REDIS_DB_SQ'], namespace: "sidekiq_#{Rails.env}" }
 end
 
 schedule_file = 'config/schedule.yml'
