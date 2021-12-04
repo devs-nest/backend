7e784a391d2d3a030d5c9dbaf48399de9a4b4acb
Vedansh Garg <vedanshkt.sfc@gmail.com>
2021-12-05 00:23:02 +0530

    adding again one more

--- devsnest/config/environments/production.rb
+++ devsnest/config/environments/production.rb
@@ -108,5 +108,6 @@ Rails.application.configure do
   config.hosts << 'api.harry.devsnest.in'
   config.hosts << 'api.ron.devsnest.in'
   config.hosts << ENV['HOSTNAME'].split('-')[0] if ENV['HOSTNAME'].present?
+  config.hosts << ENV['HOSTNAME'] if ENV['HOSTNAME'].present?
   config.hosts << 'old.devsnest.in'
 end
