f653254d5575bbab900da240b25d05eee5ee5c3e
Kshitij Dhama <kshitijdhama513@gmail.com>
2021-12-31 13:00:35 +0530

    jz rapid api (#246)
    * ⬆️ Upgrade FFI
    
    * switch to rapid jz (#245)
    
    Co-authored-by: SooditK <sooditkumarabc@gmail.com>
    Co-authored-by: Soodit Kumar <50576944+SooditK@users.noreply.github.com>

--- devsnest/app/models/algo_submission.rb
+++ devsnest/app/models/algo_submission.rb
@@ -76,7 +76,7 @@ class AlgoSubmission < ApplicationRecord
   end
 
   def self.post_to_judgez(batch)
-    jz_headers = { 'Content-Type': 'application/json', 'X-Auth-Token': ENV['JUDGEZERO_AUTH'] }
+    jz_headers = { 'Content-Type': 'application/json', 'X-Auth-Token': ENV['JUDGEZERO_AUTH'], 'x-rapidapi-host': ENV['JZ_RAPID_HOST'], 'x-rapidapi-key': ENV['JZ_RAPID_KEY']}
     response = HTTParty.post(ENV['JUDGEZERO_URL'] + '/submissions/batch?base64_encoded=true', body: batch.to_json, headers: jz_headers)
     response.read_body
     # response.code == 201 ? JSON(response.read_body) : nil
