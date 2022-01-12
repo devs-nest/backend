709a017ea819924b7a08b6f474e070e6b7682690
Yug Gurnani <66687700+Yug-gurnani@users.noreply.github.com>
2022-01-12 21:58:40 +0530

    Upgraded Puma, sidekiq and nokogiri (#242)
    * Upgraded Puma, sidekiq and nokogiri
    
    * Added changes in gemfile
    
    * Upgraded rails to 6.0.4

--- devsnest/db/schema.rb
+++ devsnest/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_13_184521) do
+ActiveRecord::Schema.define(version: 2021_12_29_184132) do
 
   create_table "algo_submissions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.integer "user_id"
