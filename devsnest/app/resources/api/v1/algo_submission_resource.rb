b13317055fafc3c5cbb1d290142cfe128d8123dd
Yug-gurnani <yug.gurnani091@gmail.com>
2022-01-27 17:09:25 +0530

    Added Score in algo_submission get api

--- devsnest/app/resources/api/v1/algo_submission_resource.rb
+++ devsnest/app/resources/api/v1/algo_submission_resource.rb
@@ -4,6 +4,11 @@ module Api
   module V1
     class AlgoSubmissionResource < JSONAPI::Resource
       attributes :user_id, :challenge_id, :source_code, :language, :test_cases, :total_test_cases, :passed_test_cases, :is_submitted, :status, :total_runtime, :total_memory
+      attributes :score_achieved
+
+      def score_achieved
+        (@model.passed_test_cases * @model.challenge.testcases.count) / @model.challenge.score
+      end
     end
   end
 end
