6b9728aced386df3fbef0fdc39f8a558c0c58589
Yug-gurnani <yug.gurnani091@gmail.com>
2022-02-01 13:38:30 +0530

    Bug fixes

--- devsnest/app/models/frontend_project.rb
+++ devsnest/app/models/frontend_project.rb
@@ -3,7 +3,7 @@
 # Project Model
 class FrontendProject < ApplicationRecord
   validates :name, uniqueness: { scope: :user_id, case_sensitive: true }
-  belongs_to :user, optional: true
+  belongs_to :user
   enum template: %i[angular react react-ts vanilla vanilla-ts vue vue3 svelte]
 
   def self.post_to_s3(user_id, frontend_project_name, file_name, raw_code)
