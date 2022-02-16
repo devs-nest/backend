b8f75946f4e91dc31cffcdf031efb142f637d15d
Yug-gurnani <yug.gurnani091@gmail.com>
2022-02-16 13:27:08 +0530

    More Rspecs added

--- devsnest/spec/models/algo_submission_spec.rb
+++ devsnest/spec/models/algo_submission_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe AlgoSubmission, type: :model do
+  let(:user) { create(:user) }
+  let(:challenge) { create(:challenge) }
+
+  it 'Should call assign score to user function' do
+
+  end
 end
