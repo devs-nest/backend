131c3f541a9a6a5e033ea17ab8e5c4b06a97db6a
Yug-gurnani <yug.gurnani091@gmail.com>
2022-03-04 18:07:02 +0530

    Bug fix

--- devsnest/app/controllers/api/v1/company_controller.rb
+++ devsnest/app/controllers/api/v1/company_controller.rb
@@ -6,13 +6,6 @@ module Api
       include JSONAPI::ActsAsResourceController
       before_action :user_auth, only: %i[index]
       before_action :admin_auth, only: %i[create]
-
-      def challenges
-        company_ids = params['data']['attributes']['company_ids']
-        company_challenges = Challenge.where(id: CompanyChallengeMapping.where(company_id: company_ids).pluck(:challenge_id))
-
-        render_success({ id: nil, type: 'challenges', challenges: company_challenges.as_json })
-      end
     end
   end
 end
