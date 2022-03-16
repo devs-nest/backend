4cee0107e1d26d15cd9753d4286dc5d16ad93145
kaydee0502 <kshitijdhama513@gmail.com>
2022-03-16 12:47:09 +0530

    score optimization

--- devsnest/spec/models/challenge_spec.rb
+++ devsnest/spec/models/challenge_spec.rb
@@ -9,7 +9,11 @@ require 'algo_templates/cpp'
 require 'sidekiq/testing'
 require 'leaderboard'
 require 'competition_ranking_leaderboard'
+<<<<<<< HEAD
 Sidekiq::Testing.fake?
+=======
+Sidekiq::Testing.fake!
+>>>>>>> score optimization
 
 RSpec.describe Challenge, type: :model do
   context 'algo template specs' do
@@ -55,14 +59,18 @@ RSpec.describe Challenge, type: :model do
     context 'cpp template' do
       let(:user) { create(:user) }
       let!(:cpp) { create(:language, judge_zero_id: 5, name: 'cpp', type_array: 'vector<_>', type_matrix: 'vector>', type_string: '_', type_primitive: '_') }
-
+      
       it 'should create a template for CPP' do
         algo_template = Templates::CPP.new(question.input_format, question.output_format)
         template = algo_template.build
         expect(template[:head]).not_to be_empty
       end
     end
+<<<<<<< HEAD
 
+=======
+    
+>>>>>>> score optimization
     context 'check competetive leaderboard' do
       let!(:user) { create(:user) }
       let!(:user2) { create(:user) }
@@ -74,7 +82,11 @@ RSpec.describe Challenge, type: :model do
       let!(:u_s2) { create(:algo_submission, user_id: user.id, challenge_id: question.id, passed_test_cases: 10, total_test_cases: 10, is_best_submission: false, is_submitted: false) }
       let!(:u2_s1) { create(:algo_submission, user_id: user2.id, challenge_id: question.id, passed_test_cases: 1, total_test_cases: 10, is_best_submission: false, is_submitted: true) }
       let!(:u2_s2) { create(:algo_submission, user_id: user2.id, challenge_id: question.id, passed_test_cases: 5, total_test_cases: 10, is_best_submission: true, is_submitted: true) }
+<<<<<<< HEAD
 
+=======
+      
+>>>>>>> score optimization
       before do
         question.regenerate_challenge_leaderboard
       end
@@ -87,6 +99,10 @@ RSpec.describe Challenge, type: :model do
         expect(ch_leaderboard.rank_for(user2.username)).to eq(3)
         expect(ch_leaderboard.score_for(user2.username)).to eq(50.0)
       end
+<<<<<<< HEAD
+=======
+      
+>>>>>>> score optimization
     end
 
     context 'check competetive leaderboard' do
@@ -100,7 +116,11 @@ RSpec.describe Challenge, type: :model do
       let!(:u_s2) { create(:algo_submission, user_id: user.id, challenge_id: question.id, passed_test_cases: 10, total_test_cases: 10, is_best_submission: false, is_submitted: false) }
       let!(:u2_s1) { create(:algo_submission, user_id: user2.id, challenge_id: question.id, passed_test_cases: 1, total_test_cases: 10, is_best_submission: false, is_submitted: true) }
       let!(:u2_s2) { create(:algo_submission, user_id: user2.id, challenge_id: question.id, passed_test_cases: 5, total_test_cases: 10, is_best_submission: true, is_submitted: true) }
+<<<<<<< HEAD
 
+=======
+      
+>>>>>>> score optimization
       before do
         question.regenerate_challenge_leaderboard
         question.update!(score: 10)
