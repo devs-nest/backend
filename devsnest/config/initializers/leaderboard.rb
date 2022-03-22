85a831734786fb1783052500da13a54166a90628
kaydee0502 <kshitijdhama513@gmail.com>
2022-03-22 13:40:04 +0530

    remove freeze

--- devsnest/config/initializers/leaderboard.rb
+++ devsnest/config/initializers/leaderboard.rb
@@ -5,7 +5,7 @@ require 'competition_ranking_leaderboard'
 module LeaderboardDevsnest
   # Initialize leaderboard with redis
   class Initializer
-    RedisConnection = { redis_connection: Redis.new(url: ENV['REDIS_HOST_LB'], password: ENV['REDIS_PASSWORD_LB'], db: ENV['REDIS_DB_LB']) }.freeze
+    RedisConnection = { redis_connection: Redis.new(url: ENV['REDIS_HOST_LB'], password: ENV['REDIS_PASSWORD_LB'], db: ENV['REDIS_DB_LB']) }
     LB = Leaderboard.new('dn_leaderboard', Devsnest::Application::REDIS_OPTIONS, RedisConnection)
   end
 
