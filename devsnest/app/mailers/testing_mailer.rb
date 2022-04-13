fc17c456f60c617243dcd78067fb00fcf569be73
Yug-gurnani <yug.gurnani091@gmail.com>
2022-04-13 12:16:38 +0530

    SendGrid Mailer with Dynamic Templates setup done

--- devsnest/app/mailers/testing_mailer.rb
+++
@@ -1,6 +0,0 @@
-class TestingMailer < ApplicationMailer
-  def send_testing_email
-    mail( :to => 'yug.gurnani091@gmail.com',
-    :subject => 'Thanks for testing up for our amazing app' )
-  end
-end
