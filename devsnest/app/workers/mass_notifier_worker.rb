28fe957e7c1ed4fc2c35b1aececb1e01ca71a216
Adhikram <adhikramm@gmail.com>
2022-04-15 11:56:11 +0530

    🔥 updates on automation

--- devsnest/app/workers/mass_notifier_worker.rb
+++ devsnest/app/workers/mass_notifier_worker.rb
@@ -13,8 +13,7 @@ class MassNotifierWorker
         next unless discord_ids.present?
 
         data = {
-          bot: bot.bot_token, message: event_message,
-          bot_id:bot.id,
+          bot_id:bot.id, message: event_message,
           discord_id: discord_ids.pluck(:discord_id)
         }
         AwsSqsWorker.perform_async('notification', data)
--- devsnest/db/schema.rb
+++ devsnest/db/schema.rb
@@ -380,6 +380,7 @@ ActiveRecord::Schema.define(version: 2022_04_27_075644) do
     t.string "bot_token"
     t.string "bot_username"
     t.boolean "is_generic_bot", default: false
+    t.boolean "is_used", default: false
   end
 
   create_table "notifications", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
