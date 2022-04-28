23b2a012f6a76bc9f9ef7ed7eec1ca4e321304a6
Kshitij Dhama <kshitijdhama513@gmail.com>
2022-04-28 13:20:38 +0530

    Groups v2 Increased group member hard limit (#309)
    * group system recreate
    
    * added checks
    
    * added member count filter
    
    * add update checks and slug
    
    * lint and rspecs
    
    * lints
    
    * increased hard group member limit
    
    * fetch only private group
    
    * merge fixes
    
    * lint fixes
    
    * back to standard error

--- devsnest/app/controllers/api/v1/groups_controller.rb
+++ devsnest/app/controllers/api/v1/groups_controller.rb
@@ -89,6 +89,8 @@ module Api
         ActiveRecord::Base.transaction do
           group.group_members.create!(user_id: user.id)
           user.update(group_assigned: true)
+          raise StandardError.new 'Group is already full!' if group.group_members.count > 16
+
           group.update!(members_count: group.members_count + 1)
         end
         api_render(200, { id: group.id, type: 'groups', slug: group.slug, message: 'Group joined' })
@@ -98,7 +100,7 @@ module Api
         user.update(group_assigned: true)
         render_error(message: 'User already in a group')
       rescue StandardError => e
-        render_error(message: "Something went wrong! : #{e}")
+        render_error(message: e)
       end
 
       def leave
