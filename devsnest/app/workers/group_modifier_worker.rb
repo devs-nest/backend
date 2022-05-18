95443fb5d4bd981a2dd10fad247ceecf740e0374
Adhikram <adhikramm@gmail.com>
2022-05-18 11:40:29 +0530

    ✨ Retry feature

--- devsnest/app/workers/group_modifier_worker.rb
+++ devsnest/app/workers/group_modifier_worker.rb
@@ -3,7 +3,7 @@
 # worker that appends tokens to a message
 class GroupModifierWorker
   include Sidekiq::Worker
-
+  sidekiq_options retry: 2
   def perform(action, group_name)
     data = {
       action: action,
