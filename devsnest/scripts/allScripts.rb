e0283439cfb7433ad37a74265a21e6e708720b1e
Adhikram <adhikramm@gmail.com>
2022-05-20 22:05:58 +0530

    ✨ 📝 scripts

---
+++ devsnest/scripts/allScripts.rb
@@ -0,0 +1,35 @@
+#  Mass Notifier Example
+MassNotifierWorker.perform_async('Hello World!')
+
+#  Role Modifier Example
+discord_id=User.first.discord_id
+RoleModifierWorker.perform_async('add_role', discord_id, 'Role Name')
+RoleModifierWorker.perform_async('delete_role', discord_id, 'Role Name')
+
+
+
+#  Mass Role Modifier Example
+discord_ids  = User.where(user_type:1).pluck(:discord_id) # get all admin discord ids
+discord_ids  = User.where(discord_active:true,accepted_in_course:true).pluck(:discord_id) # get all DN JUNE BATCH PEOPLE
+
+MassRoleModifierWorker.perform_async('add_role', discord_ids, 'TEAM NAME')
+MassRoleModifierWorker.perform_async('delete_role', discord_ids, 'TEAM NAME')
+
+#  Group Modifier Example
+GroupModifierWorker.perform_async('create', group.name)
+GroupModifierWorker.perform_async('destroy', group.name)
+
+
+#  Mass group Modifier Example
+Group.where(version:2).each do |group|
+    discord_ids=[]
+    # GroupModifierWorker.perform_async('create', group.name)
+    puts("-----------------------------------------------------")
+    puts(group.name)
+    group.group_members.each do |member|
+        user=User.find_by(id:member.user_id)
+        puts("-------------"user.name)
+        discord_ids.push(user.discord_id)
+    end
+    MassRoleModifierWorker('add_role', discord_ids, group.name)
+end
