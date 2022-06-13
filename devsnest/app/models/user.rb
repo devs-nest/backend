e0538f4873a9aab3632a729a3ff3c03aff44f62b
lakshit k. singh <66665569+LakshitXD@users.noreply.github.com>
2022-06-13 22:38:05 +0530

    User referral system (#367)
    * ✨ Referral sysytem for users
    
    * ✨ added code to show referred users
    
    * improved db validations
    
    * ⚡️ added checks
    
    * requested changes
    
    * + updated resource
    
    * ✨ added code for earned coins for users
    
    * ✨ update coin count for referral
    
    * ✨ added coins in resourse
    
    * 🐛 added validations
    
    * 🚑 fix for generating referral code
    
    * 🚑 hotfix
    
    * registration fix
    
    * name fix
    
    * rm byebug
    
    * 🚑 removed validations
    
    Co-authored-by: kaydee0502 <kshitijdhama513@gmail.com>

--- devsnest/app/models/user.rb
+++ devsnest/app/models/user.rb
@@ -45,7 +45,7 @@ class User < ApplicationRecord
   end
 
   def create_referral_code
-    Rails.logger.info 'Retrying referral code generation' while update(referral_code: SecureRandom.hex(2)) == false if web_active == true
+    Rails.logger.info 'Retrying referral code generation' while update(referral_code: SecureRandom.hex(3)) == false
   end
 
   def self.fetch_discord_id(code)
