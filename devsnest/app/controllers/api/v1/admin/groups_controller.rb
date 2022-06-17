4247160b6fe3b8da115f82453c516c9c68b2dbd4
Yug-gurnani <yug.gurnani091@gmail.com>
2022-06-17 19:00:42 +0530

    Bug fixes

--- devsnest/app/controllers/api/v1/admin/groups_controller.rb
+++ devsnest/app/controllers/api/v1/admin/groups_controller.rb
@@ -15,7 +15,7 @@ module Api
         end
 
         def merge_two_groups
-          params = params[:data][:attributes]
+          params = request.params[:data][:attributes]
           return render_error({ message: 'Group with this Name Already Exists' }) if Group.find_by(name: params[:new_group_name])
 
           if Group.merge_two_groups(params[:group_1_id], params[:group_2_id], params[:preserved_group_id], { name: params[:new_group_name], owner_id: params[:owner_id], co_owner_id: params[:co_owner_id] })
