2d56bc6a3200fb4b7f8d1222fde15ac4d9c784c4
lakshit k. singh <66665569+LakshitXD@users.noreply.github.com>
2022-06-18 13:26:05 +0530

    🐛 added fix for group messges spam (#376)

--- devsnest/app/models/group_member.rb
+++ devsnest/app/models/group_member.rb
@@ -27,6 +27,7 @@ class GroupMember < ApplicationRecord
   def send_scrum_message_in_group
     message = "Hope you are enjoying the Server! \nYou can connect with your group here, We encourage you to meet tomorrow for your first meeting and get to know each other at 7pm\nThe agenda of the meeting will be \n\n1. Get to know each other\n2 To decide a daily catchup time that works for you all \n3. Choose your goals for the course\n4. Talk to your team and  team leader and see how all you can manage responsibilities together\n5. Nominate yourself if you want to become Vice team leader, and let the team leader ping in the admins"
     group = Group.find_by(id: group_id)
-    GroupNotifierWorker.perform_async([group.name], message, group.server&.guild_id) if group.group_members.count == 5
+    GroupNotifierWorker.perform_async([group.name], message, group.server&.guild_id) if group.group_members.count == 5 && group.five_members_flag == false
+    group.update(five_members_flag: true)
   end
 end
