442f5b169a22da2690d25485d9fc4aaac938c493
LakshitXD <lakshitk6666@gmail.com>
2022-06-20 13:22:25 +0530

    removed pagination

--- devsnest/app/resources/api/v1/group_resource.rb
+++ devsnest/app/resources/api/v1/group_resource.rb
@@ -6,7 +6,6 @@ module Api
     class GroupResource < JSONAPI::Resource
       attributes :name, :owner_id, :co_owner_id, :members_count, :student_mentor_id, :owner_name, :co_owner_name, :batch_leader_id, :slug, :created_at, :user_group, :group_type, :language,
                  :classification, :description, :version, :server_link
-      paginator :paged
       has_many :group_members
       filter :classification
       filter :language
