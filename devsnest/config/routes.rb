48b1bcf89bcd614bb68c1764b616afee742062b0
LakshitXD <lakshitk6666@gmail.com>
2022-06-23 19:39:14 +0530

    removed show for companies

--- devsnest/config/routes.rb
+++ devsnest/config/routes.rb
@@ -135,7 +135,7 @@ Rails.application.routes.draw do
       end
       jsonapi_resources :certification, only: %i[show]
       jsonapi_resources :frontend_project, only: %i[show index create update destroy]
-      jsonapi_resources :company, only: %i[index create show]
+      jsonapi_resources :company, only: %i[index create]
       jsonapi_resources :discussion, only: %i[show index create destroy]
       jsonapi_resources :upvote, only: %i[create destroy]
       jsonapi_resources :college_form, only: %i[create]
