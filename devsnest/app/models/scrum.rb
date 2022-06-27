d9ef53bc70d3ea32cc6c0f940f53cfe4c95446a9
kaydee0502 <kshitijdhama513@gmail.com>
2022-06-27 12:00:41 +0530

    skrrrr

--- devsnest/app/models/scrum.rb
+++ devsnest/app/models/scrum.rb
@@ -11,8 +11,10 @@ class Scrum < ApplicationRecord
   def handle_manual_update(params, group, user)
     if group.admin_rights_auth(user)
       params = (params.dig 'data', 'attributes').except(:user_id, :group_id, :creation_date)
+      params = params.permit(:attendance, :saw_last_lecture, :tha_progress, :topics_to_cover, :backlog_reasons, :class_rating)
     else
       params = (params.dig 'data', 'attributes').except(:user_id, :group_id, :creation_date, :attendance)
+      params = params.permit(:saw_last_lecture, :tha_progress, :topics_to_cover, :backlog_reasons, :class_rating)
     end
     update!(params)
   end
