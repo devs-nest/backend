cc131548d154b6f60fbf8b1a4c8e747aa52e8a32
kaydee0502 <kshitijdhama513@gmail.com>
2022-07-27 15:27:40 +0530

    specs

--- devsnest/spec/factories.rb
+++ devsnest/spec/factories.rb
@@ -9,6 +9,8 @@ FactoryBot.define do
   end
 
   factory :judgeztoken do
+    sequence(:submission_id) { |n| n }
+    sequence(:token) { |n| n }
   end
 
   factory :discussion do
@@ -30,6 +32,11 @@ FactoryBot.define do
     sequence(:challenge_id) { |n| n }
     sequence(:is_best_submission) { |_n| false }
     sequence(:is_submitted) { |_n| true }
+  end
+
+  factory :run_submission do
+    sequence(:user_id) { |n| n }
+    sequence(:challenge_id) { |n| n }
   end
 
   factory :frontend_question do
