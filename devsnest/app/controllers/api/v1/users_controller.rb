a9fcee06f320c1dbe1a822cf773aedf8769ed051
Ankit Prasad <ankkyprasad@gmail.com>
2022-08-22 23:21:02 +0530

    group feature enabled

--- devsnest/app/controllers/api/v1/users_controller.rb
+++ devsnest/app/controllers/api/v1/users_controller.rb
@@ -167,8 +167,6 @@ module Api
         return render_error({ message: "Discord isn't connected" }) unless @current_user.discord_active
 
         return render_error({ message: 'User already in a group' }) if GroupMember.find_by(user_id: @current_user.id)
-
-        @current_user.update!(accepted_in_course: true)
         
         @current_user.update!(updatable_params[:attributes])
         render_success({ message: 'Form filled' })
