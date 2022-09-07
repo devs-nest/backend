770d353f7718dc3f5fe61457599682b5cfd4fcec
kaydee0502 <kshitijdhama513@gmail.com>
2022-09-07 14:20:38 +0530

    additional logic

--- devsnest/app/controllers/api/v1/coin_logs_controller.rb
+++ devsnest/app/controllers/api/v1/coin_logs_controller.rb
@@ -10,12 +10,13 @@ module Api
         data = params.dig(:data, :attributes)
         user = User.find_by(id: data[:user_id])
 
-        return render_error("message": "Invalid User") if user.blank?
-
+        return render_error("message": "User not found") if user.blank?
         coins = data[:coins].to_i
+        return render_error("message": "Insufficient coins") if user.score < coins 
         coins *= -1 if coins.positive?
-
+        
         CoinLog.create(pointable_type: "Session", coins: coins, user_id: user.id)
+        user.update(coins: user.coins - coins)
 
         render_success({ id: user.id, type: 'session', message: "Points reedemed for session" })
       end
