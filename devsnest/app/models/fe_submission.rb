cafdbf7ec7cb7657065be65f3dda2572de59122c
Yug-gurnani <yug.gurnani091@gmail.com>
2022-09-09 00:02:00 +0530

    Fixed bugs related to assign score

--- devsnest/app/models/fe_submission.rb
+++ devsnest/app/models/fe_submission.rb
@@ -7,14 +7,11 @@ class FeSubmission < ApplicationRecord
   after_save :assign_score, if: :saved_change_to_passed_test_cases?
 
   def assign_score
-    frontend_challenge = FrontendChallenge.find_by_id(frontend_challenge_id)
-    return nil if frontend_challenge.blank?
-
-    frontend_challenge.update!(score: score)
+    # self.update!(score: score) TODO
     frontend_challenge_score = FrontendChallengeScore.find_by(user_id: user_id, frontend_challenge_id: frontend_challenge_id)
 
     if frontend_challenge_score.nil?
-      FrontendChallengeScore.create!(user_id: user_id, frontend_challenge_id: frontend_challenge.id,
+      FrontendChallengeScore.create!(user_id: user_id, frontend_challenge_id: frontend_challenge_id,
                                      total_test_cases: total_test_cases, passed_test_cases: passed_test_cases, score: score)
     elsif frontend_challenge_score.score < score
       frontend_challenge_score.update!(score: score, fe_submission_id: id, passed_test_cases: passed_test_cases, total_test_cases: total_test_cases)
