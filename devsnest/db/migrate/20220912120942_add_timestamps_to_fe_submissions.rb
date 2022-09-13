04c59a3c22bf6b3545a344a6b032634085efaa8f
Adhikram Maitra <adhikramm@gmail.com>
2022-09-13 18:04:12 +0530

    🐛 Working version

--- devsnest/db/migrate/20220912120942_add_timestamps_to_fe_submissions.rb
+++ devsnest/db/migrate/20220912120942_add_timestamps_to_fe_submissions.rb
@@ -1,7 +1,5 @@
 class AddTimestampsToFeSubmissions < ActiveRecord::Migration[6.0]
-  change_table :fe_submissions do |t|
-    t.timestamps default: Time.current
-    t.change_default :created_at, from: Time.current, to: nil
-    t.change_default :updated_at, from: Time.current, to: nil
+  def change
+    add_timestamps :fe_submissions , null: true
   end
 end
