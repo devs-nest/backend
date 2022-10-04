03df4ce45ce39a1b90a758e2d486d39acd42a805
Lakshit K Singh <lakshit.singh@gogroup.co>
2022-10-04 11:32:05 +0530

    lmao fix

--- devsnest/app/resources/api/v1/organization_resource.rb
+++ devsnest/app/resources/api/v1/organization_resource.rb
@@ -2,6 +2,8 @@
 
 module Api
   module V1
-    attributes :name, :description, :slug, :website, :logo_banner, :logo, :heading, :created_at, :updated_at
+    class OrganizationResource < JSONAPI::Resource
+      attributes :name, :description, :slug, :website, :logo_banner, :logo, :heading, :created_at, :updated_at
+    end
   end
 end
