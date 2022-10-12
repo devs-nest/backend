6cf06400a34341fad700d2add7d8f47e636d03d3
Lakshit K Singh <lakshit.singh@gogroup.co>
2022-10-12 13:20:49 +0530

    multiple upload and logo added for skill

--- devsnest/app/controllers/api/v1/skill_controller.rb
+++ devsnest/app/controllers/api/v1/skill_controller.rb
@@ -5,6 +5,15 @@ module Api
     class SkillController < ApplicationController
       include JSONAPI::ActsAsResourceController
       before_action :admin_auth, only: %i[create update destroy]
+
+
+      def create
+        skills = params[:data][:attributes][:name]
+        skills.each do |skill_name, logo|
+          Skill.create(name: skill_name, logo: logo) if Skill.find_by(name: skill_name).nil?
+        end
+        render_success({ message: 'Skills created successfully' })
+      end
     end
   end
 end
--- devsnest/db/schema.rb
+++ devsnest/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_07_070429) do
+ActiveRecord::Schema.define(version: 2022_10_12_073737) do
 
   create_table "algo_submissions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.integer "user_id"
@@ -617,6 +617,7 @@ ActiveRecord::Schema.define(version: 2022_10_07_070429) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "logo"
   end
 
   create_table "submissions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
