600e2932b083610710efd5b8c056777d63de4bab
Adhikram Maitra <adhikramm@gmail.com>
2022-10-13 14:44:51 +0530

    ✨ Code Climate

--- devsnest/app/workers/group_activity_points_update_worker.rb
+++ devsnest/app/workers/group_activity_points_update_worker.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
+# worker to update group activity points every week
 class GroupActivityPointsUpdateWorker
   include Sidekiq::Worker
-  # worker to update group activity points every week
   def perform
     Group.all.each do |group|
       group.update(activity_point: group.count_activity_point)
--- devsnest/spec/workers/group_activity_points_update_worker_spec.rb
+++ devsnest/spec/workers/group_activity_points_update_worker_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 RSpec.describe GroupActivityPointsUpdateWorker, type: :worker do
   context 'When group activity points update worker is called' do
