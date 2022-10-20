d4545cfb558f32b5fb633d495feb295456e5591f
kaydee0502 <kshitijdhama513@gmail.com>
2022-10-20 11:09:22 +0530

    add 422

--- devsnest/app/controllers/api/v1/admin/listmonk_controller.rb
+++ devsnest/app/controllers/api/v1/admin/listmonk_controller.rb
@@ -14,7 +14,7 @@ module Api
           return render_error('No condition given') if condition.blank?
 
           res = eval(condition)
-          return render_unprocessable unless res.try(:sample).is_a?(User)
+          return render_unprocessable("Query doesn't return User object") unless res.try(:sample).is_a?(User)
 
           response.headers['Content-Type'] = 'text/csv; charset=UTF-8; header=present'
           response.headers['Content-Disposition'] = 'attachment; filename=list_result.csv'
