241d9cd7bcc6912732105be74992b2f0f9db1c91
Adhikram Maitra <adhikramm@gmail.com>
2022-11-07 11:33:56 +0530

    Dummy commit

--- devsnest/app/workers/email_sender_worker.rb
+++ devsnest/app/workers/email_sender_worker.rb
@@ -6,7 +6,6 @@ class EmailSenderWorker
 
   def perform(receiver_email, meta_data, template_id)
     SendgridMailer.send(receiver_email, meta_data, template_id)
-    # puts("Mail sent to #{receiver_email} with template_id #{template_id} and meta_data #{meta_data}")
     User.find_by(email: receiver_email)&.update(accepted_in_course: true) if meta_data['user_accepted']
   end
 end
