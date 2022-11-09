41b930fe0b8516ecd9b6c950429ffcab1082ccd5
Lakshit K Singh <lakshit.singh@gogroup.co>
2022-11-09 10:07:44 +0530

    rubocop chnages

--- devsnest/app/controllers/api/v1/article_controller.rb
+++ devsnest/app/controllers/api/v1/article_controller.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 module Api
   module V1
+    # Article Controller
     class ArticleController < ApplicationController
       include JSONAPI::ActsAsResourceController
       before_action :user_auth, only: %i[create_submission]
--- devsnest/db/schema.rb
+++ devsnest/db/schema.rb
@@ -125,7 +125,7 @@ ActiveRecord::Schema.define(version: 2022_11_04_075034) do
   create_table "certifications", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.integer "user_id"
     t.string "certificate_type"
-    t.string "cuid", default: "6sbhpnqcCp8"
+    t.string "cuid", default: "0yPXYShcthc"
     t.string "title", default: ""
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -155,11 +155,12 @@ ActiveRecord::Schema.define(version: 2022_11_04_075034) do
   end
 
   create_table "coin_logs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
     t.string "pointable_type"
     t.integer "pointable_id"
     t.integer "coins", default: 0
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "college_forms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
@@ -469,7 +470,6 @@ ActiveRecord::Schema.define(version: 2022_11_04_075034) do
     t.string "slug"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["organization_id"], name: "index_jobs_on_organization_id"
     t.index ["slug"], name: "index_jobs_on_slug", unique: true
   end
 
@@ -581,6 +581,13 @@ ActiveRecord::Schema.define(version: 2022_11_04_075034) do
   create_table "referrals", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.integer "referred_user_id"
     t.string "referral_code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "rewards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
