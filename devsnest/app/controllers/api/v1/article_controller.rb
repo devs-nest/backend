37d601e161332235575a5bec0b30e182f9564ca0
Lakshit K Singh <lakshit.singh@gogroup.co>
2022-11-09 10:50:46 +0530

    review changes

--- devsnest/app/controllers/api/v1/article_controller.rb
+++ devsnest/app/controllers/api/v1/article_controller.rb
@@ -9,16 +9,17 @@ module Api
 
       def fetch_by_slug
         article = Article.find_by(slug: params[:slug])
-        user_submission = article&.article_submissions&.find_by(user_id: @current_user&.id)
         return render_not_found('article') if article.nil?
+
+        user_submission = article.article_submissions&.find_by(user_id: @current_user&.id)
 
         render_success(article.as_json.merge(user_submission: user_submission&.slice(:submission_link)))
       end
 
       def create_submission
         article_id = params['data']['attributes']['article_id']
-        if ArticleSubmission.find_by(user_id: @current_user.id, article_id: article_id).present?
-          article_submission = ArticleSubmission.find_by(user_id: @current_user.id, article_id: article_id)
+        article_submission = ArticleSubmission.find_by(user_id: @current_user.id, article_id: article_id)
+        if article_submission.present?
           article_submission.update(submission_link: params['data']['attributes']['submission_link'])
         else
           article_submission = ArticleSubmission.new(article_id: article_id, user_id: @current_user.id, submission_link: params['data']['attributes']['submission_link'])
