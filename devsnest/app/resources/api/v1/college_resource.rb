9db7608000e3652643cda67963f9a3a8df1240a7
kaydee0502 <kshitijdhama513@gmail.com>
2022-11-15 14:10:32 +0530

    rm add fiels from show

--- devsnest/app/resources/api/v1/college_resource.rb
+++ devsnest/app/resources/api/v1/college_resource.rb
@@ -3,15 +3,7 @@
 module Api
   module V1
     class CollegeResource < JSONAPI::Resource
-      attributes :name, :is_verified, :invites, :members
-
-      def invites
-        @model.college_invites.as_json
-      end
-
-      def members
-        @model.college_profiles.where(department: context[:user].college_profile.department).where.not(user_id: nil).as_json
-      end
+      attributes :name, :is_verified
     end
   end
 end
