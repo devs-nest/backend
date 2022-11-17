4e19d1be1989938bceb387df24ccd83c974ad575
Yug-gurnani <yug.gurnani091@gmail.com>
2022-11-17 14:05:02 +0530

    Added Contect in be challenge controller

--- devsnest/app/controllers/api/v1/backend_challenge_controller.rb
+++ devsnest/app/controllers/api/v1/backend_challenge_controller.rb
@@ -6,6 +6,13 @@ module Api
     class BackendChallengeController < ApplicationController
       include JSONAPI::ActsAsResourceController
       before_action :user_auth, only: %i[index show]
+
+      def context
+        {
+          user: @current_user,
+          action: params[:action]
+        }
+      end
     end
   end
 end
