c4e07c968e3d3086682779814874132da0c56de1
Ankit Prasad <ankkyprasad@gmail.com>
2022-11-19 08:44:41 +0530

    be_submission pagination and filter

--- devsnest/app/resources/api/v1/be_submission_resource.rb
+++ devsnest/app/resources/api/v1/be_submission_resource.rb
@@ -5,6 +5,8 @@ module Api
     # Be resource
     class BeSubmissionResource < JSONAPI::Resource
       attributes :user_id, :backend_challenge_id, :submitted_url, :status, :score, :total_test_cases, :passed_test_cases
+      filter :user_id
+      paginator :paged
     end
   end
 end
