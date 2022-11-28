5aa819d5fa8dfbfdb638912fb5a11066752881aa
Yug-gurnani <yug.gurnani091@gmail.com>
2022-11-28 22:15:51 +0530

    Added Timestamps in be submission

--- devsnest/app/resources/api/v1/be_submission_resource.rb
+++ devsnest/app/resources/api/v1/be_submission_resource.rb
@@ -4,7 +4,7 @@ module Api
   module V1
     # Be resource
     class BeSubmissionResource < JSONAPI::Resource
-      attributes :user_id, :backend_challenge_id, :submitted_url, :status, :score, :total_test_cases, :passed_test_cases, :passed_test_cases_desc, :failed_test_cases_desc
+      attributes :user_id, :backend_challenge_id, :submitted_url, :status, :score, :total_test_cases, :passed_test_cases, :passed_test_cases_desc, :failed_test_cases_desc, :created_at, :updated_at
       filter :user_id
       filter :backend_challenge_id
     end
