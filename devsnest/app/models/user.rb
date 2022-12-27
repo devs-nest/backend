c6a59ccf8216efb671ad93285aaf8df86092afa2
Adhikram Maitra <47442228+Adhikram@users.noreply.github.com>
2022-12-27 13:11:09 +0530

    🐛 Fixed (#537)

--- devsnest/app/models/user.rb
+++ devsnest/app/models/user.rb
@@ -555,10 +555,10 @@ class User < ApplicationRecord
       user = User.find_by_id(id)
       {
         name: user.name,
-        dsa_solved: user.solved,
-        dsa_solved_by_difficulty: user.total_by_difficulty,
-        fe_solved: user.fe_solved,
-        fe_solved_by_topic: user.fe_total_by_topic,
+        dsa_solved: Challenge.count_solved(user.id),
+        dsa_solved_by_difficulty: Challenge.split_by_difficulty,
+        fe_solved: FrontendChallenge.count_solved(user.id),
+        fe_solved_by_topic: FrontendChallenge.split_by_topic,
         tha_details: user.tha_details, # Bootcamp Progress
         leaderboard_details: user.leaderboard_details('dsa'),
         fe_leaderboard_details: user.leaderboard_details('frontend')
