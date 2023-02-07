6d0e2ea21f89b6f2c22aa80f91328b28a1b05606
Adhikram Maitra <adhikramm@gmail.com>
2023-02-07 11:59:02 +0530

    🚑

--- devsnest/app/workers/streak_changer_worker.rb
+++ devsnest/app/workers/streak_changer_worker.rb
@@ -5,12 +5,10 @@ class StreakChangesWorker
   include Sidekiq::Worker
   sidekiq_options retry: 5
   def perform
-    users = User.where('web_active = true and dsa_streak > 0')
+    users = User.where('web_active = true and dsa_streak > 0 and streak_end_date < ?', 1.day.ago)
     users.each do |user|
-      if user.streak_end_date < 1.day.ago && user.dsa_streak.positive?
-        user.update(last_dsa_streak: user.dsa_streak)
-        user.update(dsa_streak: 0)
-      end
+      user.update(last_dsa_streak: user.dsa_streak)
+      user.update(dsa_streak: 0)
     end
   end
 end
