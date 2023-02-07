08ac101061f6f754ef5ac0a97b12da3bca64b965
Adhikram Maitra <adhikramm@gmail.com>
2023-02-07 11:54:52 +0530

    🔥 updates

--- devsnest/app/workers/streak_changer_worker.rb
+++ devsnest/app/workers/streak_changer_worker.rb
@@ -7,8 +7,8 @@ class StreakChangesWorker
   def perform
     users = User.where('web_active = true and dsa_streak > 0')
     users.each do |user|
-      if user.streak_end_date < 1.day.ago
-        user.update(last_streak: user.dsa_streak)
+      if user.streak_end_date < 1.day.ago && user.dsa_streak.positive?
+        user.update(last_dsa_streak: user.dsa_streak)
         user.update(dsa_streak: 0)
       end
     end
--- devsnest/app/workers/streak_notifier_worker.rb
+++ devsnest/app/workers/streak_notifier_worker.rb
@@ -5,13 +5,13 @@ class StreakNotifierWorker
   include Sidekiq::Worker
   sidekiq_options retry: 5
   def perform
-    users = User.where('web_active = true and dsa_streak = 0 and streak_end_date between  ? and ?', 4.day.ago, 3.day.ago)
-    template_id = EmailTemplate.find_by(name: 'registration_mail')&.template_id
+    users = User.where('web_active = true and dsa_streak = 0 and last_dsa_streak > 0 and streak_end_date between  ? and ?', 2.day.ago, 1.day.ago)
+    template_id = EmailTemplate.find_by(name: 'streak_notifier')&.template_id
     users.each do |user|
       EmailSenderWorker.perform_async(user.email, {
                                         'unsubscribe_token': user.unsubscribe_token,
                                         'username': user.username,
-                                        'last_streak': user.last_streak,
+                                        'last_streak': user.last_dsa_streak,
                                         'streak_end_date': user.streak_end_date
                                       }, template_id)
     end
