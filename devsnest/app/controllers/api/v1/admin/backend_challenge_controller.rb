9d5e0f2be0901df3df48e1d409de0ae0549e4b31
Ankit Prasad <ankkyprasad@gmail.com>
2023-02-13 13:45:50 +0530

    files deletion fix

--- devsnest/app/controllers/api/v1/admin/backend_challenge_controller.rb
+++ devsnest/app/controllers/api/v1/admin/backend_challenge_controller.rb
@@ -27,14 +27,14 @@ module Api
           challenge = BackendChallenge.find_by(id: params['id'])
           return render_not_found('challenge') if challenge.nil?
 
-          return render_error('invalid challenge type') if challenge.challenge_type == 'normal'
+          if challenge.challenge_type == 'stackblitz'
+            bucket = 'backend-testcases'
+            files = params.dig(:data, :attributes, 'files')
+            return if files.nil?
 
-          bucket = 'backend-testcases'
-          files = params.dig(:data, :attributes, 'files')
-          return if files.nil?
-
-          io_boilerplate(files, challenge.id.to_s, bucket)
-          params['data']['attributes'].delete('files')
+            io_boilerplate(files, challenge.id.to_s, bucket)
+            params['data']['attributes'].delete('files')
+          end
         end
 
         def remove_files
@@ -44,8 +44,19 @@ module Api
           if challenge.challenge_type == 'stackblitz'
             bucket = 'backend-testcases'
             files = $s3.list_objects(bucket: "#{ENV['S3_PREFIX']}#{bucket}", prefix: "#{challenge.id}/")
-            io_boilerplate(files, '', bucket, 'remove')
+            io_boilerplate(files[:contents], '', bucket, 'remove')
           end
+        end
+
+        def files_io
+          challenge = BackendChallenge.find_by(id: params.dig(:data, :attributes, :id))
+          return render_not_found('challenge') if challenge.nil?
+
+          bucket = 'backend-testcases'
+          key = "#{challenge.id}/#{params.dig(:data, :attributes, :filename)}"
+          remove_test_cases(bucket, key)
+
+          render_success({ message: 'Updated files' })
         end
       end
     end
