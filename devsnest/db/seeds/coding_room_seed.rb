99264a6a9354349e60b4204ebd9d56a9db8b06b1
Adhikram Maitra <47442228+Adhikram@users.noreply.github.com>
2023-02-17 13:21:37 +0530

    Cr fix 3 (#559)
    * ⚡️
    
    * 🔥 Tested Version

--- devsnest/db/seeds/coding_room_seed.rb
+++ devsnest/db/seeds/coding_room_seed.rb
@@ -7,7 +7,7 @@ Challenge.create(user: User.first, name: 'test1', topic: 'arrays', is_active: tr
 Challenge.create(user: User.first, name: 'test2', topic: 'tree', is_active: true, score: 100)
 Challenge.create(user: User.first, name: 'test3', topic: 'hashmap', is_active: true, score: 100)
 
-CodingRoom.create(name: 'DN-Team', room_time: 1, challenge_list: [1,2,3], finish_at: Time.now + 60.minutes, user_id: User.first.id, starts_at: Time.now, difficulty: '[\'easy\']', question_count: 3, topics: '[\'array\']"')
+CodingRoom.create(name: 'DN-Team', room_time: 0, challenge_list: [1,2,3], user_id: User.first.id, starts_at: Time.now, difficulty: ["easy"].to_json, question_count: 3, topics: ["array"].to_json)
 
 CodingRoomUserMapping.create(user_id: User.last.id, coding_room_id: CodingRoom.last.id)
 CodingRoomUserMapping.create(user_id: 1, coding_room_id: CodingRoom.last.id)
@@ -20,5 +20,6 @@ AlgoSubmission.create(user_id: User.last.id, challenge_id: Challenge.first.id, l
 AlgoSubmission.create(user_id: User.last.id, challenge_id: Challenge.second.id, language: 'cpp', status: 'Accepted', passed_test_cases: 10, total_test_cases: 10, is_submitted: true,coding_room_id: CodingRoom.last.id)
 AlgoSubmission.create(user_id: User.last.id, challenge_id: Challenge.third.id, language: 'cpp', status: 'Accepted', passed_test_cases: 10, total_test_cases: 10, is_submitted: true, coding_room_id: CodingRoom.last.id)
 AlgoSubmission.create(user_id: User.last.id, challenge_id: Challenge.second.id, language: 'cpp', status: 'Accepted', passed_test_cases: 10, total_test_cases: 10, is_submitted: true,coding_room_id: CodingRoom.last.id)
+
 lb = LeaderboardDevsnest::RoomLeaderboard.new(CodingRoom.last.id.to_s).call
 lb.leaders(2, with_member_data: true)
