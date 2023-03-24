ee148544b4073edfa13795b4357ddadf964dbf00
Ankit Prasad <ankkyprasad@gmail.com>
2023-03-24 15:16:07 +0530

    users can see multiple groups

--- devsnest/app/resources/api/v1/group_resource.rb
+++ devsnest/app/resources/api/v1/group_resource.rb
@@ -86,11 +86,7 @@ module Api
         if options[:context][:is_create] || options[:context][:slug].present? || options[:context][:group_id].present? || options[:context][:user]&.is_admin?
           super(options)
         else
-          user_group = GroupMember.find_by(user_id: options[:context][:user]&.id)&.group
-          return super(options).where(id: user_group.id) if user_group.present?
-
           Group.eligible_groups.order('activity_point desc, ((members_count%15) - (members_count)%5)/10 desc , (members_count%15)%5')
-
         end
       end
     end
