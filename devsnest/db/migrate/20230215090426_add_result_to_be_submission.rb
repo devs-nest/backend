37bd691fe66783f97f1b9bc5cffe829a9742a024
Ankit Prasad <ankkyprasad@gmail.com>
2023-03-27 14:11:44 +0530

    merged migrations into one

--- devsnest/db/migrate/20230215090426_add_result_to_be_submission.rb
+++
@@ -1,5 +0,0 @@
-class AddResultToBeSubmission < ActiveRecord::Migration[6.0]
-  def change
-    add_column :be_submissions, :result, :text
-  end
-end
--- devsnest/db/migrate/20230217052018_create_projects.rb
+++ devsnest/db/migrate/20230217052018_create_projects.rb
@@ -8,5 +8,19 @@ class CreateProjects < ActiveRecord::Migration[6.0]
 
       t.timestamps
     end
+
+    add_column :be_submissions, :result, :text
+
+    add_column :backend_challenges, :active_path, :string
+    add_column :backend_challenges, :files, :text
+    add_column :backend_challenges, :folder_name, :string
+    add_column :backend_challenges, :hidden_files, :text
+    add_column :backend_challenges, :open_paths, :text
+    add_column :backend_challenges, :protected_paths, :text
+    add_column :backend_challenges, :template, :string
+    add_column :backend_challenges, :challenge_type, :integer, default: 0
+
+    add_column :backend_challenges, :is_project, :boolean, default: false
+    add_column :frontend_challenges, :is_project, :boolean, default: false
   end
 end
