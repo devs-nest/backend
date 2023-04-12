4a68eca658e91913365027d121b128017d5dafef
Ankit Prasad <ankkyprasad@gmail.com>
2023-04-12 19:00:47 +0530

    initial commit

---
+++ devsnest/spec/requests/admin/backend_challenge_spec.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Api::V1::Admin::BackendChallengeController, type: :request do
+  let(:user) { create(:user, name: 'user1', user_type: 1) }
+  let(:be_challenge) { create(:backend_challenge, user_id: user.id, name: 'todo list', challenge_type: 'stackblitz') }
+
+  before :each do
+    sign_in(user)
+  end
+
+  context 'GET - backend challenges' do
+    it 'should return all the backend challenge' do
+      get '/api/v1/admin/backend-challenge'
+      expect(response.status).to eq(200)
+    end
+
+    it 'should return a backend challenge when id is provided' do
+      get "/api/v1/admin/backend-challenge/#{be_challenge.id}"
+      expect(response.status).to eq(200)
+    end
+
+    it 'should not return a backend challenge when the challenge is not found' do
+      get '/api/v1/admin/backend-challenge/1201'
+      expect(response.status).to eq(404)
+    end
+
+    it 'should return all the active challenges' do
+      get '/api/v1/admin/backend-challenge/active_questions'
+      expect(response.status).to eq(200)
+    end
+  end
+
+  context 'DELETE - backend challenge' do
+    it 'should delete backend challenge with specified id' do
+      delete "/api/v1/admin/backend-challenge/#{be_challenge.id}"
+      expect(response.status).to eq(204)
+    end
+
+    it 'should not delete backend challenge if its not found' do
+      delete "/api/v1/admin/backend-challenge/#{be_challenge.id}00000"
+      expect(response.status).to eq(404)
+    end
+  end
+end
---
+++ devsnest/spec/requests/be_submission_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Api::V1::BeSubmissionsController, type: :request do
+  let(:user) { create(:user) }
+  let(:be_challenge) { create(:backend_challenge, user_id: user.id, name: 'todo list', challenge_type: 'stackblitz') }
+  let(:submission) { create(:be_submission, user_id: user.id, backend_challenge_id: be_challenge.id) }
+
+  before :each do
+    sign_in(user)
+  end
+
+  context 'GET - Backend challenge Submission' do
+    it 'should get all submission' do
+      get '/api/v1/be-submissions'
+      expect(response.status).to eq(200)
+    end
+
+    it 'should get submission with specified id' do
+      get "/api/v1/be-submissions/#{submission.id}"
+      expect(response.status).to eq(200)
+    end
+
+    it 'should not return submission when it is not found' do
+      get "/api/v1/be-submissions/#{submission.id}000"
+      expect(response.status).to eq(404)
+    end
+  end
+end
