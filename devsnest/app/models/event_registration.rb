c59e26da69df48b7ecc34965d167e53c2be92715
Yug-gurnani <yug.gurnani091@gmail.com>
2023-05-31 14:56:57 +0530

    Added Mails After Event Registration

--- devsnest/app/models/event_registration.rb
+++ devsnest/app/models/event_registration.rb
@@ -14,4 +14,18 @@
 class EventRegistration < ApplicationRecord
   belongs_to :edu_event
   belongs_to :user
+
+  after_create :send_event_registration_mail
+
+  def send_event_registration_mail
+    if edu_event.organizer == 'Solana'
+      template_id = EmailTemplate.find_by(name: 'solana_registration_lm').try(:template_id)
+      return if template_id.blank?
+
+      EmailSenderWorker.perform_async(user.email, {
+                                        'username': user.name.blank? ? user.username : user.name.titleize
+                                      },
+                                      template_id)
+    end
+  end
 end
