3841345e70d5e5b30f3be820b6c2e570fc8c60c3
Adhikram Maitra <47442228+Adhikram@users.noreply.github.com>
2023-07-07 20:04:01 +0530

    💄 Fixed Backward Dependency (#606)

--- devsnest/app/controllers/api/v1/event_registrations_controller.rb
+++ devsnest/app/controllers/api/v1/event_registrations_controller.rb
@@ -16,6 +16,7 @@ module Api
         return render_unprocessable('User Already Registered.') if EventRegistration.find_by(user_id: @current_user.id, edu_event_id: edu_event_id).present?
 
         EventRegistration.create!(user_id: @current_user.id, edu_event_id: edu_event_id, user_data: user_data)
+        @current_user.update!(accepted_in_course: true, is_fullstack_course_22_form_filled: true) # TODO: remove this line after frontend fix there code
         render_success({ message: 'Registered Successfully.' })
       end
     end
