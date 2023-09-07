5438b7dbf63ef88c7baae709d32c56667a57f198
Ankit Prasad <ankkyprasad@gmail.com>
2023-09-07 21:54:20 +0530

    added dn_airtribe flag to users

--- devsnest/db/schema.rb
+++ devsnest/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_07_095019) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_07_160405) do
   create_table "algo_submissions", charset: "utf8mb3", force: :cascade do |t|
     t.integer "user_id"
     t.integer "challenge_id"
@@ -192,7 +192,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_07_095019) do
   create_table "certifications", charset: "utf8mb3", force: :cascade do |t|
     t.integer "user_id"
     t.string "certificate_type"
-    t.string "cuid", default: "L65cJ00wsZY"
+    t.string "cuid", default: "MXTEB22NhzM"
     t.string "title", default: ""
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -1043,6 +1043,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_07_095019) do
     t.date "streak_end_date"
     t.integer "last_dsa_streak", default: 0
     t.boolean "is_college_student", default: false
+    t.boolean "dn_airtribe_student", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
