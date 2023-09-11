d20aa31e738001533d515a72c0d8e4adb0898b26
Ankit Prasad <ankkyprasad@gmail.com>
2023-09-11 14:40:34 +0530

    added course name in bootcamp progress

--- devsnest/app/resources/api/v1/bootcamp_progress_resource.rb
+++ devsnest/app/resources/api/v1/bootcamp_progress_resource.rb
@@ -5,7 +5,7 @@ module Api
     # Bootcamp Progress resource
     class BootcampProgressResource < JSONAPI::Resource
       attributes :user_id, :course_id, :course_curriculum_id, :completed, :created_at, :updated_at
-      attributes :course_type
+      attributes :course_type, :course_name
 
       def self.creatable_fields(context)
         super - [:completed]
@@ -13,6 +13,10 @@ module Api
 
       def course_type
         @model.course_curriculum.try(:course_type)
+      end
+
+      def course_name
+        @model.course.name
       end
     end
   end
