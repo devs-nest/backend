175a257cdd3c58aef8045dba7a04a9aa983c4009
lakshit k. singh <66665569+LakshitXD@users.noreply.github.com>
2023-10-04 23:31:18 +0530

    lmao caught in 4k

--- devsnest/app/workers/user_weekly_activity_worker.rb
+++ devsnest/app/workers/user_weekly_activity_worker.rb
@@ -10,16 +10,17 @@ class UserWeeklyActivityWorker
     User.where.not(id: unsub_user_ids).where(web_active: true).each do |user|
       dashboard_data = user.get_dashboard_by_cache
 
-    EmailSenderWorker.perform_async(user.email, {
-                                      'unsubscribe_token': user.unsubscribe_token,
-                                      'username': user.username,
-                                      'dsa_rank': dashboard_data[:leaderboard_details][:rank],
-                                      'dsa_solved': dashboard_data[:dsa_solved].values.sum,
-                                      'total_dsa': dashboard_data[:dsa_solved_by_difficulty].values.sum,
-                                      'frontend_rank': dashboard_data[:fe_leaderboard_details][:rank],
-                                      'frontend_solved': dashboard_data[:fe_solved].values.sum,
-                                      'total_frontend': dashboard_data[:fe_solved_by_topic].values.sum,
-                                      'mass_emailer': true
-                                    }, template_id)
+      EmailSenderWorker.perform_async(user.email, {
+                                        'unsubscribe_token': user.unsubscribe_token,
+                                        'username': user.username,
+                                        'dsa_rank': dashboard_data[:leaderboard_details][:rank],
+                                        'dsa_solved': dashboard_data[:dsa_solved].values.sum,
+                                        'total_dsa': dashboard_data[:dsa_solved_by_difficulty].values.sum,
+                                        'frontend_rank': dashboard_data[:fe_leaderboard_details][:rank],
+                                        'frontend_solved': dashboard_data[:fe_solved].values.sum,
+                                        'total_frontend': dashboard_data[:fe_solved_by_topic].values.sum,
+                                        'mass_emailer': true
+                                      }, template_id)
+    end
   end
 end
