3d4eb01fbe07a4ec3c224dcdbd37a12b2d2fc836
Kshitij Dhama <kshitijdhama513@gmail.com>
2023-10-31 17:08:12 +0530

    dw

--- devsnest/app/controllers/api/v1/users_controller.rb
+++ devsnest/app/controllers/api/v1/users_controller.rb
@@ -180,7 +180,13 @@ module Api
         if user.present?
           sign_in(user)
           set_current_user
-          return render_success(user.as_json.merge({ "type": 'users' })) if @current_user.present?
+          colleges = user.college_profile.includes(:college).collect do |cps| {
+            id: cps.college&.id,
+            slug: cps.college&.slug,
+            name: cps.college&.name
+          }
+
+          return render_success(user.as_json.merge({ "type": 'users', "colleges": colleges })) if @current_user.present?
         end
         render_error({ message: 'Error occured while authenticating' })
       end
