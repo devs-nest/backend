d6832af3f2624221768bd67e7d4454deb4a1d50f
Sam <gurnani.yug091@gmail.com>
2024-01-22 17:59:31 +0530

    Expire cache

--- devsnest/app/models/sql_submission.rb
+++ devsnest/app/models/sql_submission.rb
@@ -4,4 +4,9 @@
 class SqlSubmission < ApplicationRecord
   belongs_to :user
   belongs_to :sql_challenge
+  after_commit :expire_cache
+
+  def expire_cache
+    Rails.cache.delete("user_sql_submission_#{user_id}_#{sql_challenge_id}")
+  end
 end
